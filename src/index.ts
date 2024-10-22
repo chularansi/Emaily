be0409a90dd3acbb95336fb2c9e072d4582a4029
Chularansi Fernando <chularansifernando@yahoo.com>
2024-10-22 17:14:48 +0200

    added initial router

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 // import usersRouter from './routes/users';
 
 const app = express();
 
 // app.use('/api/users', usersRouter);
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Hi there');
+});
 
 const PORT = process.env.PORT || 5000;
 
