cf118ba0abf424fd0ba2fe9a763a94153f4561c8
Chularansi Fernando <chularansifernando@yahoo.com>
2024-10-22 17:22:33 +0200

    run build script

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,10 @@ const express_1 = __importDefault(require("express"));
 // import usersRouter from './routes/users';
 const app = (0, express_1.default)();
 // app.use('/api/users', usersRouter);
-const PORT = 5000;
+app.get('/', (req, res) => {
+    res.status(200).send('Hi there');
+});
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is listning on port ${PORT}`);
 });
